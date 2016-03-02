3b27aadf167e70183d44572d9371a465a0a4a8e2
Arron Dick <s_drac2@yahoo.com>
2016-03-02 14:27:50 +1300

    Inital set up of three activities

---
+++ MultipleActivities/app/src/main/java/bit/dickaj1/multipleactivities/ActivityA.java
@@ -0,0 +1,15 @@
+package bit.dickaj1.multipleactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all);
+    }
+
+
+}
