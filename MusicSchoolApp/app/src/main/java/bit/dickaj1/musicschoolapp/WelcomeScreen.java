d0e08f62909840573836cf1f9faaf53199b0e297
Arron Dick <s_drac2@yahoo.com>
2016-03-02 21:26:20 +1300

    Added button click and intent change

--- MusicSchoolApp/app/src/main/java/bit/dickaj1/musicschoolapp/WelcomeScreen.java
+++ MusicSchoolApp/app/src/main/java/bit/dickaj1/musicschoolapp/WelcomeScreen.java
@@ -1,11 +1,13 @@
 package bit.dickaj1.musicschoolapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class WelcomeScreen extends AppCompatActivity {
 
@@ -14,6 +16,21 @@ public class WelcomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
 
+        //Grab handle of the button
+        Button buttonContinue = (Button)findViewById(R.id.buttonWelcome);
+
+        //Set event handler
+        buttonContinue.setOnClickListener(new buttonContinueHandler());
     }
+     public class buttonContinueHandler implements View.OnClickListener{
+
+         @Override
+         public void onClick(View v) {
+             //Create Intent
+             Intent changeActivityIntent = new Intent(WelcomeScreen.this,MainActivity.class);
+             //Transfer control to the next activity
+             startActivity(changeActivityIntent);
+         }
+     }
 
 }
