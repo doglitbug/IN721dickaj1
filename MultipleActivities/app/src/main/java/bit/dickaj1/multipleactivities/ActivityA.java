c2624d89f75d5d67dc842a09b1a495e5c3a703c2
Arron Dick <s_drac2@yahoo.com>
2016-03-02 14:49:57 +1300

    Activity A changing instruction text

--- MultipleActivities/app/src/main/java/bit/dickaj1/multipleactivities/ActivityA.java
+++ MultipleActivities/app/src/main/java/bit/dickaj1/multipleactivities/ActivityA.java
@@ -1,7 +1,11 @@
 package bit.dickaj1.multipleactivities;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityA extends AppCompatActivity {
 
@@ -9,7 +13,34 @@ public class ActivityA extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all);
+
+        //Set up the instructions
+        //Get textView
+        TextView textViewInstructions = (TextView)findViewById(R.id.textViewInstructions);
+        //Get instructional text
+        Resources res=getResources();
+        String instructions=res.getString(R.string.instructionsA);
+        //Set on textView
+        textViewInstructions.setText(instructions);
+
+
+        //Set up the button
+        //Grab the handle of the button
+        Button btnChange = (Button)findViewById(R.id.btnChange);
+
+        //Set event handler
+        btnChange.setOnClickListener(new btnChangeClickHandler());
     }
 
+ public class btnChangeClickHandler implements View.OnClickListener{
+     /**
+      * Deal with a single click
+      * @param v The view that was clicked
+      */
+     @Override
+     public void onClick(View v) {
+
+     }
+ }
 
 }
--- MultipleActivities/app/src/main/java/bit/dickaj1/multipleactivities/ActivityC.java
+++ MultipleActivities/app/src/main/java/bit/dickaj1/multipleactivities/ActivityC.java
@@ -1,7 +1,9 @@
 package bit.dickaj1.multipleactivities;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ActivityC extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class ActivityC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all);
+
+        //Set up the instructions
+        //Get textView
+        TextView textViewInstructions = (TextView)findViewById(R.id.textViewInstructions);
+        //Get instructional text
+        Resources res=getResources();
+        String instructions=res.getString(R.string.instructionsC);
+        //Set on textView
+        textViewInstructions.setText(instructions);
     }
 }
