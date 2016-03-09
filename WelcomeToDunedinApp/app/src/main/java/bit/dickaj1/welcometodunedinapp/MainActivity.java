f10c8bff1e4b8c27e9851eacb9ade5076abf5484
Arron Dick <s_drac2@yahoo.com>
2016-03-09 20:41:56 +1300

    Changed to OnItemClick

--- WelcomeToDunedinApp/app/src/main/java/bit/dickaj1/welcometodunedinapp/MainActivity.java
+++ WelcomeToDunedinApp/app/src/main/java/bit/dickaj1/welcometodunedinapp/MainActivity.java
@@ -3,6 +3,8 @@ package bit.dickaj1.welcometodunedinapp;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,24 +15,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Populate listViewMain
+        //Populate listViewCategories and set handler
         setUpListViewCategories();
-
-        //Create onClickHandler for listViewMain
     }
 
+    /**
+     * Sets up the listView with the pages that can be selected
+     * and sets on click handler
+     */
     private void setUpListViewCategories(){
-        //Get list of categories
-        Resources res=getResources();
-        String pages[] = res.getStringArray(R.array.categories);
-
-        //Get reference to listViewMain
+        //Get reference to listViewCategories
         ListView listViewCategories=(ListView)findViewById(R.id.listViewCategories);
+
+        //Get list of categories for res
+        Resources res=getResources();
+        String categories[] = res.getStringArray(R.array.categories);
+
         //Get a layout
         int layoutID=R.layout.categories_layout;
 
         //Create the adapter
-        ArrayAdapter<String>categoriesAdapter=new ArrayAdapter<String>(this,layoutID,pages);
+        ArrayAdapter<String>categoriesAdapter=new ArrayAdapter<String>(this,layoutID,categories);
+        //Set adapter
         listViewCategories.setAdapter(categoriesAdapter);
+
+        //Set up handler
+        listViewCategories.setOnItemClickListener(new listViewCategoriesHandler());
+    }
+
+    /**
+     * Sets up the onClick handler for the listView
+     */
+    public class listViewCategoriesHandler implements ListView.OnItemClickListener{
+
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            
+        }
     }
 }
