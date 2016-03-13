d313d82ce06e57aed9cc178db3ecf3b57f7b0fbc
Arron Dick <s_drac2@yahoo.com>
2016-03-14 12:47:17 +1300

    Set up button handlers

--- FragmentsIntro/app/src/main/java/bit/dickaj1/fragmentsintro/MainActivity.java
+++ FragmentsIntro/app/src/main/java/bit/dickaj1/fragmentsintro/MainActivity.java
@@ -2,6 +2,8 @@ package bit.dickaj1.fragmentsintro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set up button handlers
+        setUpButtonHandlers();
+    }
+
+    private void setUpButtonHandlers(){
+        //Grab handle of buttons
+        Button btnShowImageView=(Button)findViewById(R.id.btnShowImageView);
+        Button btnShowListView=(Button)findViewById(R.id.btnShowListView);
+        //Set event handler
+        btnShowImageView.setOnClickListener(new btnShowImageViewHandler());
+        btnShowListView.setOnClickListener(new btnShowListViewHandler());
+    }
+
+    private class btnShowImageViewHandler implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            //TODO Show Image view fragment
+        }
+    }
+
+    private class btnShowListViewHandler implements View.OnClickListener{
+        @Override
+        public void onClick(View v){
+            //TODO Show list view fragment
+        }
     }
 }
--- FragmentsIntro/app/src/main/java/bit/dickaj1/fragmentsintro/ShowListViewFragment.java
+++ FragmentsIntro/app/src/main/java/bit/dickaj1/fragmentsintro/ShowListViewFragment.java
@@ -1,20 +1,30 @@
 package bit.dickaj1.fragmentsintro;
 
 import android.app.Fragment;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class ShowListViewFragment extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
-        View fragmentView= inflater.inflate(R.layout.show_listview_fragment,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
+        View fragmentView = inflater.inflate(R.layout.show_listview_fragment, container, false);
 
-        // Do any required setup for thr fragment view here...
-        //TODO load up listview
-
+        //1 .Get a reference to the list view with fragmentView.findViewbyId
+        ListView lvCities = (ListView) fragmentView.findViewById(R.id.lvCities);
+        //2. Get your array of string from Res
+        Resources res = getResources();
+        String[] cityNamesArray = res.getStringArray(R.array.city_names_array);
+        //3. Create the adapter
+        ArrayAdapter<String> cityNamesAdapter=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,cityNamesArray);
+        //4. Pass it to setAdapter to bind it
+        lvCities.setAdapter(cityNamesAdapter);
+        //Return finished view
         return fragmentView;
     }
 }
