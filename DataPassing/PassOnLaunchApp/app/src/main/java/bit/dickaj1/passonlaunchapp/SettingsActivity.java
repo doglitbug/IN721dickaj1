ffba91dc488b4c433162c0a614571ea74a379f60
Arron Dick <s_drac2@yahoo.com>
2016-03-16 14:43:34 +1300

    Set up design of settings and default code(button click and change intent)

--- DataPassing/PassOnLaunchApp/app/src/main/java/bit/dickaj1/passonlaunchapp/SettingsActivity.java
+++ DataPassing/PassOnLaunchApp/app/src/main/java/bit/dickaj1/passonlaunchapp/SettingsActivity.java
@@ -13,5 +13,30 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        setupButton();
+    }
+
+    /**
+     * Set up the on click handler for the settings button
+     */
+    public void setupButton() {
+        //Grab reference to button
+        Button btnSettings = (Button) findViewById(R.id.buttonReturn);
+        //Set up button handler
+        btnSettings.setOnClickListener(new btnSettingsHandler());
+    }
+
+    /**
+     * Handler for settings button
+     */
+    public class btnSettingsHandler implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            //Create intent
+            Intent changeActivityIntent = new Intent(SettingsActivity.this, MainActivity.class);
+            //Transfer control to the new activity
+            startActivity(changeActivityIntent);
+        }
     }
 }
