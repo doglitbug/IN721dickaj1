07d97522d9b3eee13990e316c6c659652a862c61
Arron Dick <s_drac2@yahoo.com>
2016-03-16 14:39:17 +1300

    Finished code for main activity

--- DataPassing/PassOnLaunchApp/app/src/main/java/bit/dickaj1/passonlaunchapp/SettingsActivity.java
+++ DataPassing/PassOnLaunchApp/app/src/main/java/bit/dickaj1/passonlaunchapp/SettingsActivity.java
@@ -1,7 +1,10 @@
 package bit.dickaj1.passonlaunchapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
     }
 }
