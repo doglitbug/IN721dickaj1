727dcf52ad570f0ab45df247173c7af16348dd3d
Arron Dick <s_drac2@yahoo.com>
2016-03-17 21:27:42 +1300

    Tea

--- DataPassing/RequestingDataApp/app/src/main/java/bit/dickaj1/requestingdataapp/MainActivity.java
+++ DataPassing/RequestingDataApp/app/src/main/java/bit/dickaj1/requestingdataapp/MainActivity.java
@@ -1,7 +1,10 @@
 package bit.dickaj1.requestingdataapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set up button and handler
+        setupButton();
+    }
+
+    /**
+     * Set up the on click handler for the settings button
+     */
+    public void setupButton(){
+        //Grab reference to button
+        Button btnSettings = (Button)findViewById(R.id.buttonChange);
+        //Set up button handler
+        btnSettings.setOnClickListener(new btnSettingsHandler());
+    }
+
+    /**
+     * Handler for settings button
+     */
+    public class btnSettingsHandler implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            //Create intent
+            Intent changeActivityIntent = new Intent(MainActivity.this,SettingsActivity.class);
+            //Transfer control to the new activity
+            startActivityForResult(changeActivityIntent,42);
+        }
     }
 }
--- DataPassing/RequestingDataApp/app/src/main/java/bit/dickaj1/requestingdataapp/SettingsActivity.java
+++ DataPassing/RequestingDataApp/app/src/main/java/bit/dickaj1/requestingdataapp/SettingsActivity.java
@@ -1,7 +1,10 @@
 package bit.dickaj1.requestingdataapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        returnControl();
+    }
+
+    public void returnControl(){
+        //Create an intent
+        Intent returnIntent=new Intent();
+        //Get data to return
+        int dataToReturn=getTextViewColor();
+        //Stuff it into 
+    }
+
+    /**
+     * Find the color of the text in the textview
+     * @return int representing the color
+     */
+    public int getTextViewColor(){
+        Color theColor;
+        //Get reference
+        TextView theTextView=(TextView)findViewById(R.id.textViewBodySettings);
+        //Get color and return
+        return theTextView.getCurrentTextColor();
     }
 }
