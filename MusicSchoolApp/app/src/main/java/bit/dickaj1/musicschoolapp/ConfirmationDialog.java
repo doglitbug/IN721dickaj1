f83c762d92981ab9cd99ed9b0dc37400aa051cfa
Arron Dick <s_drac2@yahoo.com>
2016-03-18 17:44:12 +1300

    Added button handlers to dialog

--- MusicSchoolApp/app/src/main/java/bit/dickaj1/musicschoolapp/ConfirmationDialog.java
+++ MusicSchoolApp/app/src/main/java/bit/dickaj1/musicschoolapp/ConfirmationDialog.java
@@ -3,6 +3,7 @@ package bit.dickaj1.musicschoolapp;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 /**
@@ -18,11 +19,30 @@ public class ConfirmationDialog extends DialogFragment {
 
         builder.setIcon(R.drawable.ic_music);
         builder.setTitle("Confirm?");
-        builder.setPositiveButton("Yes",new YesButtonHandler());
+        builder.setPositiveButton("Yes", new YesButtonHandler());
         builder.setNegativeButton("No",new NoButtonHandler());
 
         Dialog customDialog=builder.create();
 
         return customDialog;
     }
+
+    public class YesButtonHandler implements DialogInterface.OnClickListener{
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            MainActivity myActivity=(MainActivity) getActivity();
+            myActivity.GiveMeMyData(true);
+        }
+    }
+
+    public class NoButtonHandler implements DialogInterface.OnClickListener{
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            MainActivity myActivity=(MainActivity) getActivity();
+            myActivity.GiveMeMyData(false);
+        }
+    }
+
 }
