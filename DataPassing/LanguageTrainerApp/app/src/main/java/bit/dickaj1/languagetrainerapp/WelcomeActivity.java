a5433f303ab4cfa5b6b8df7798321c3df34ac826
Arron Dick <s_drac2@yahoo.com>
2016-03-25 15:15:23 +1300

    Rough version on welcome screen, button to start questions

--- DataPassing/LanguageTrainerApp/app/src/main/java/bit/dickaj1/languagetrainerapp/WelcomeActivity.java
+++ DataPassing/LanguageTrainerApp/app/src/main/java/bit/dickaj1/languagetrainerapp/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package bit.dickaj1.languagetrainerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //Create button handler etc
+        //Grab reference
+        Button btnStart=(Button)findViewById(R.id.btnStart);
+        //Set event handler
+        btnStart.setOnClickListener(new btnStartClickHandler());
+    }
+
+    public class btnStartClickHandler implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            //Create Intent
+            Intent startIntent = new Intent(WelcomeActivity.this,QuestionsActivity.class);
+            //TODO Bundle in the question manager or default data(so that activity knows to
+            //Start questions from the start
+            startActivity(startIntent);
+        }
     }
 }
