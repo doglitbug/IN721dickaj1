9c98a673836682e2b367aa70a776b0f3c4083331
Arron Dick <s_drac2@yahoo.com>
2016-03-25 15:18:51 +1300

    Set up question activity skeleton and linked welcome to switch to it

---
+++ DataPassing/LanguageTrainerApp/app/src/main/java/bit/dickaj1/languagetrainerapp/QuestionsActivity.java
@@ -0,0 +1,16 @@
+package bit.dickaj1.languagetrainerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+        //TODO check intent to see if this is the start of question time or in the middle of such
+        //TODO retrieve question manager etc
+    }
+}
