3cc30fe3f4b493a582a717b8027da9c856294717
Arron Dick <s_drac2@yahoo.com>
2016-04-08 21:47:07 +1200

    Added display of results and finished task

--- ExternalStorage/SQLite/app/src/main/java/bit/dickaj1/sqlite/MainActivity.java
+++ ExternalStorage/SQLite/app/src/main/java/bit/dickaj1/sqlite/MainActivity.java
@@ -3,7 +3,10 @@ package bit.dickaj1.sqlite;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -30,7 +33,21 @@ public class MainActivity extends AppCompatActivity {
      * Sets up handlers for the buttons
      */
     private void setUpButtons(){
-        //TODO Finish
+        //Get reference to the button
+        Button btnSearch=(Button)findViewById(R.id.btnSearch);
+        //Set onclick handler
+        btnSearch.setOnClickListener(new btnSearchHandler());
+    }
+
+    public class btnSearchHandler implements Button.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            //Get country
+            Spinner spinnerCountries=(Spinner)findViewById(R.id.spinnerCountries);
+            String country=spinnerCountries.getSelectedItem().toString();
+            //Display data
+            displayCities(country);
+        }
     }
 
     /**
@@ -47,5 +64,21 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String>spinnerCountriesAdapter=new ArrayAdapter<>(this,layoutId,data);
         //Bind the adapter
         spinnerCountries.setAdapter(spinnerCountriesAdapter);
+    }
+
+    /**
+     * Search for country and display in listView
+     * @param country to search for
+     */
+    private void displayCities(String country){
+        //Get reference to listView
+        ListView lvCities = (ListView)findViewById(R.id.lvCities);
+        //Get the data for listview
+        ArrayList<String> data = db.searchCountry(country);
+        //Create the adapter
+        ArrayAdapter<String> citiesAdapter=new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,data);
+
+        //Set adapter
+        lvCities.setAdapter(citiesAdapter);
     }
 }
