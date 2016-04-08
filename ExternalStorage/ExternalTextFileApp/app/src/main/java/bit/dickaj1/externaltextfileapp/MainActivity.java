51dc48b92f4575e22318f530c78a9358eae8dc90
Arron Dick <s_drac2@yahoo.com>
2016-04-08 16:00:15 +1200

    LIstView being loaded and displayed from asset

--- ExternalStorage/ExternalTextFileApp/app/src/main/java/bit/dickaj1/externaltextfileapp/MainActivity.java
+++ ExternalStorage/ExternalTextFileApp/app/src/main/java/bit/dickaj1/externaltextfileapp/MainActivity.java
@@ -1,9 +1,19 @@
 package bit.dickaj1.externaltextfileapp;
 
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Sets up handlers for buttons
      */
-    private void setUpButton(){
+    private void setUpButton() {
         //Grab reference to button
-        Button btnFill=(Button)findViewById(R.id.btnFill);
+        Button btnFill = (Button) findViewById(R.id.btnFill);
         //Set onclick handler
         btnFill.setOnClickListener(new btnFillHandler());
     }
@@ -29,18 +39,66 @@ public class MainActivity extends AppCompatActivity {
     /**
      * On click handler for btnFill
      */
-    public class btnFillHandler implements Button.OnClickListener{
+    public class btnFillHandler implements Button.OnClickListener {
 
         @Override
         public void onClick(View v) {
-            fillList();
+            //Holds data
+            ArrayList<String> data;
+            //Fetch data
+            data = getList();
+            //Display on screen
+            displayList(data);
         }
     }
 
     /**
-     * Load and populate the list
+     * Put the provided data into a list
+     *
+     * @param data ArrayList of data to display
      */
-    private void fillList(){
-        //TODO populate list
+    private void displayList(ArrayList<String> data) {
+        //Grab reference to the listView
+        ListView lvCities=(ListView)findViewById(R.id.lvCities);
+
+        //Create the adapter
+        ArrayAdapter<String> citiesAdapter=new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,data);
+
+        //Set adapter
+        lvCities.setAdapter(citiesAdapter);
+    }
+
+    /**
+     * Grabs data from asset
+     *
+     * @return ArrayList of data to place in list
+     */
+    private ArrayList<String> getList() {
+        //Hold data to return
+        ArrayList<String> data = new ArrayList<>();
+
+        //Name of asset
+        String filename = "city_names.txt";
+
+        //Get Asset manager
+        AssetManager am = getAssets();
+
+        //Hold a line of input
+        String line;
+
+        try {
+            InputStream is = am.open(filename);
+            InputStreamReader ir = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(ir);
+
+            while ((line = br.readLine()) != null) {
+                data.add(line);
+            }
+
+            br.close();
+        } catch (IOException e) {
+            //TODO Throw a fit?
+        }
+        return data;
     }
 }
