b735cf62da4d5609a21be26ece34e20ae7dac919
Arron Dick <s_drac2@yahoo.com>
2016-04-08 15:09:20 +1200

    Added all code for button clicks

--- ExternalStorage/ExternalTextFileApp/app/src/main/java/bit/dickaj1/externaltextfileapp/MainActivity.java
+++ ExternalStorage/ExternalTextFileApp/app/src/main/java/bit/dickaj1/externaltextfileapp/MainActivity.java
@@ -2,6 +2,8 @@ package bit.dickaj1.externaltextfileapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Set up button handler
-        //TODO
+        setUpButton();
+    }
 
+    /**
+     * Sets up handlers for buttons
+     */
+    private void setUpButton(){
+        //Grab reference to button
+        Button btnFill=(Button)findViewById(R.id.btnFill);
+        //Set onclick handler
+        btnFill.setOnClickListener(new btnFillHandler());
+    }
+
+    /**
+     * On click handler for btnFill
+     */
+    public class btnFillHandler implements Button.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            fillList();
+        }
+    }
+
+    /**
+     * Load and populate the list
+     */
+    private void fillList(){
+        //TODO populate list
     }
 }
