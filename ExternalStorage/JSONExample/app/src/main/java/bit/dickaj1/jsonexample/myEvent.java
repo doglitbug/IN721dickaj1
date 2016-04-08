49d7edf2dbaa94419514165a21e866f706a0671c
Arron Dick <s_drac2@yahoo.com>
2016-04-08 22:52:14 +1200

    Added myEvent class to hold a name and description for an event, can be expanded to include other data eg, url or image

---
+++ ExternalStorage/JSONExample/app/src/main/java/bit/dickaj1/jsonexample/myEvent.java
@@ -0,0 +1,19 @@
+package bit.dickaj1.jsonexample;
+
+/**
+ * Created by Arron on 8/04/2016.
+ */
+public class myEvent {
+    String name;
+    String description;
+
+    public myEvent(String name, String description){
+        this.name=name;
+        this.description=description;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}
