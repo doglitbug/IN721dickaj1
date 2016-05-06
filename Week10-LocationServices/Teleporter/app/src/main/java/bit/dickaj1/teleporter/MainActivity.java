cd6fe5d276b7bc06595749218876705045ad117a
Arron Dick <s_drac2@yahoo.com>
2016-05-06 14:39:14 +1200

    Set button to generate and display random location values

--- Week10-LocationServices/Teleporter/app/src/main/java/bit/dickaj1/teleporter/MainActivity.java
+++ Week10-LocationServices/Teleporter/app/src/main/java/bit/dickaj1/teleporter/MainActivity.java
@@ -4,7 +4,9 @@ import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -15,12 +17,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Debugging
-        Location test=getLocation();
-        Log.i("ABC123", "onCreate: "+test.toString());
+        //Set up button handlers
+        setUpButtons();
     }
 
+    /**
+     * Sets up the on click handler for button(s)
+     */
+    private void setUpButtons(){
+    //Get reference to button
+    Button btnTeleport=(Button)findViewById(R.id.btnTeleport);
 
+    //Set onclick handler
+    btnTeleport.setOnClickListener(new btnTeleportHandler());
+}
+
+    private class btnTeleportHandler implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            //Get a new location
+            Location test=getLocation();
+
+            //Set on form
+            updateLocation(test);
+        }
+    }
+
+    /**
+     * Updates the screen to show the current location
+     * @param newLocation Location to show
+     */
+    private void updateLocation(Location newLocation){
+        //Get references
+        TextView latitude=(TextView)findViewById(R.id.tvLatitude);
+        TextView longitude=(TextView)findViewById(R.id.tvLongitude);
+        //Set text
+        latitude.setText(String.format("%.3f",newLocation.getLatitude()));
+        longitude.setText(String.format("%.3f",newLocation.getLongitude()));
+    }
     /**
      * Gets/generates a location
      * @return Location requested
