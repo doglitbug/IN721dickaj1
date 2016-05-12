2db562582b8c02c554b24606c8f65ddb3ecaa92d
doglitbug <dickaj1@student.op.ac.nz>
2016-05-13 00:03:39 +1200

    Figured out that onCreate was destroying global varibles. Stored serilized File object in bundle for usage with onSaveInstanceState

--- Week11.1-Camera/PhotoMosaic/app/src/main/java/bit/dickaj1/photomosaic/MainActivity.java
+++ Week11.1-Camera/PhotoMosaic/app/src/main/java/bit/dickaj1/photomosaic/MainActivity.java
@@ -2,6 +2,7 @@ package bit.dickaj1.photomosaic;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -10,21 +11,39 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    //Used for debugging
     String TAG="ABC123";
+
+    //Record where we tell the camera to put the photo
+    File imageFile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setUpButtons();
+
+        //Restore imageFile if available
+        if (savedInstanceState!=null){
+            imageFile=(File)savedInstanceState.get("imageFile");
+        }
     }
 
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceData) {
+        super.onSaveInstanceState(savedInstanceData);
+        //Save imageFile
+        savedInstanceData.putSerializable("imageFile", imageFile);
+    }
     /**
      * Initializes button click handlers
      */
@@ -49,52 +68,72 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Start up the camera to get a photo
      */
-    private void startCamera(){
+    private void startCamera() {
         //Get new file object
-        File mPhotoFile=getNewFile();
+        getNewFile();
 
         //Get Uri of file
-        Uri mPhotoUri= Uri.fromFile(mPhotoFile);
+        Uri mPhotoUri = Uri.fromFile(imageFile);
 
         //Create and intent to start camera
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
         //Attach Uri to the intent(tell camera where to store the photo taken
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,mPhotoUri);
-        Log.i(TAG, "startCamera: file path: "+mPhotoFile.toString());
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mPhotoUri);
+
         //Launch the intent, waiting for result
-        startActivityForResult(cameraIntent,1);
+        startActivityForResult(cameraIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Is this the return we are waiting for?
+        if (requestCode == 1) {
+            //Did we get the data?
+            if (resultCode == RESULT_OK) {
+
+                //Find out file path of saved image
+                String imageFilePath = imageFile.getPath();
+
+                //Make bitmap from that file path
+                Bitmap userPhotoBitmap = BitmapFactory.decodeFile(imageFilePath);
+
+                //Pass off to setMosaic to manipulate and display
+                setMosaic(userPhotoBitmap);
+
+            } else {
+                Toast.makeText(MainActivity.this, "No photo saved. Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     //////////// Non form related stuff ////////////
     /**
      * Create a File object to write an image to
-     * @return Unique File object
      */
-    private File getNewFile(){
+    private void getNewFile() {
         //Fetch system image folder
         File imageFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
 
         //Make subdirectory
-        File myImageFolder=new File(imageFolder,getString(R.string.app_name));
+        File myImageFolder = new File(imageFolder, getString(R.string.app_name));
+
         //Check folder exists(needs to be done every time in case user deletes it while app is running
-        if (!myImageFolder.exists()){
+        if (!myImageFolder.exists()) {
             myImageFolder.mkdirs(); //mkdirs creates parent folders as well
         }
 
         //generate timestamp for file name
-        SimpleDateFormat timeStampFormat= new SimpleDateFormat("yyyyMMdd_HHmmss");
-        Date currentTime=new Date();
-        String timeStamp=timeStampFormat.format(currentTime);
+        SimpleDateFormat timeStampFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Date currentTime = new Date();
+        String timeStamp = timeStampFormat.format(currentTime);
 
         //Make file name
 
-        String mPhotoFileName="IMG_"+timeStamp+".jpg";
+        String mPhotoFileName = "IMG_" + timeStamp + ".jpg";
 
         //Make complete File object(path and filename)
-        File photoFile=new File(imageFolder.getPath()+File.separator+mPhotoFileName);
-
-        return photoFile;
+        imageFile = new File(myImageFolder.getPath() + File.separator + mPhotoFileName);
     }
 
     /**
@@ -102,9 +141,11 @@ public class MainActivity extends AppCompatActivity {
      * @param input image to use for mosaic
      */
     private void setMosaic(Bitmap input){
-        if (input==null){
-            //TODO Deal with null image being provided
-        }
-        //TODO The photo mosaic stuff and display it
+        //TODO Turn image into a mosaic
+
+        //Get reference
+        ImageView ivMosaic=(ImageView)findViewById(R.id.ivMosaic);
+        //Display it
+        ivMosaic.setImageBitmap(input);
     }
 }
