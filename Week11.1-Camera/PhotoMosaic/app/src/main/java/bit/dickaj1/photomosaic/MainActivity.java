2f82e1e602cf67cd2104c0263d2999e01517a1c4
doglitbug <dickaj1@student.op.ac.nz>
2016-05-12 22:20:11 +1200

    Button launches Camera

--- Week11.1-Camera/PhotoMosaic/app/src/main/java/bit/dickaj1/photomosaic/MainActivity.java
+++ Week11.1-Camera/PhotoMosaic/app/src/main/java/bit/dickaj1/photomosaic/MainActivity.java
@@ -1,21 +1,26 @@
 package bit.dickaj1.photomosaic;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setUpButtons();
-
-        //TODO Set up file path etc
     }
 
     /**
@@ -31,12 +36,63 @@ public class MainActivity extends AppCompatActivity {
     /**
      * On click handler for btnTakePhoto
      */
-    public class btnTakePhotoHandler implements View.OnClickListener{
+    private class btnTakePhotoHandler implements View.OnClickListener{
 
         @Override
         public void onClick(View v) {
-            //TODO launch camera intent to get photo
+            startCamera();
         }
+    }
+
+    /**
+     * Start up the camera to get a photo
+     */
+    private void startCamera(){
+        //Get new file object
+        File mPhotoFile=getNewFile();
+
+        //Get Uri of file
+        Uri mPhotoUri= Uri.fromFile(mPhotoFile);
+
+        //Create and intent to start camera
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //Attach Uri to the intent(tell camera where to store the photo taken
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,mPhotoUri);
+
+        //Launch the intent, waiting for result
+        startActivityForResult(cameraIntent,1);
+    }
+
+    //////////// Non form related stuff ////////////
+    /**
+     * Create a File object to write an image to
+     * @return Unique File object
+     */
+    private File getNewFile(){
+        //Fetch system image folder
+        File imageFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        //Make subdirectory
+        File myImageFolder=new File(imageFolder,getString(R.string.app_name));
+        //Check folder exists(needs to be done every time in case user deletes it while app is running
+        if (!myImageFolder.exists()){
+            myImageFolder.mkdirs(); //mkdirs creates parent folders as well
+        }
+
+        //generate timestamp for file name
+        SimpleDateFormat timeStampFormat= new SimpleDateFormat("yyyyMMdd_HHmmss");
+        Date currentTime=new Date();
+        String timeStamp=timeStampFormat.format(currentTime);
+
+        //Make file name
+
+        String mPhotoFileName="IMG_"+timeStamp+".jpg";
+
+        //Make complete File object(path and filename)
+        File photoFile=new File(imageFolder.getPath()+File.separator);
+
+        return photoFile;
     }
 
     /**
