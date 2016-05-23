8f49ca9cd4e0c956abd62c0d02861fad2c0f1469
doglitbug <dickaj1@student.op.ac.nz>
2016-05-23 16:39:38 +1200

    Changed getCoords to use a LatLng to work with Google Map

--- Week12.2-3rdPartyLibraries/TeleporterPlus/app/src/main/java/bit/dickaj1/teleporterplus/MainActivity.java
+++ Week12.2-3rdPartyLibraries/TeleporterPlus/app/src/main/java/bit/dickaj1/teleporterplus/MainActivity.java
@@ -1,9 +1,14 @@
 package bit.dickaj1.teleporterplus;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.google.android.gms.maps.model.LatLng;
+
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +30,27 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             //TODO Get current GPS co-ords
-
+            LatLng testLocation = getCoords();
             //TODO Create map object using Google Maps API
 
             //TODO Show on screen
         }
+    }
+
+    /**
+     * Gets/generates a LatLng
+     *
+     * @return Location requested
+     */
+    private LatLng getCoords() {
+        Random rng = new Random();
+        //Generate a random latitude between -90 and +90
+        double latitude =rng.nextDouble() * 180 - 90;
+        //Generate a random longitude between -180 and +180
+        double longitude =rng.nextDouble() * 360 - 180;
+
+        //Construct new LatLng object to return
+        LatLng output = new LatLng(latitude, longitude);
+        return output;
     }
 }
